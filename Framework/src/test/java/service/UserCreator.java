79eda0e8439913a7a64a52c457d7ef38c0f7d7ae
Osipenko Anton <hguihggjjh@gmail.com>
2020-12-24 04:40:37 +0300

    two functional tests with edits in the code

--- Framework/src/test/java/service/UserCreator.java
+++ Framework/src/test/java/service/UserCreator.java
@@ -8,14 +8,14 @@ public class UserCreator {
     public static final String TESTDATA_USER_REGISTRATIONEMAIL = "testdata.user.registrationEmail";
     public static final String TESTDATA_USER_PASSWORD = "testdata.user.password";
 
-    public static User withCredentialsFromProperty(){
+    public static User WithLowLimitSymbols(){
         return new User( TestDataReader.getTestData(TESTDATA_USER_NAME),
                 TestDataReader.getTestData(TESTDATA_USER_LASTNAME),
                 TestDataReader.getTestData(TESTDATA_USER_REGISTRATIONEMAIL),
                 TestDataReader.getTestData(TESTDATA_USER_PASSWORD));
     }
 
-    public static User WithLowLimitSymbols(){
-    return new User(TestDataReader.getTestData(TESTDATA_USER_NAME), TestDataReader.getTestData(TESTDATA_USER_LASTNAME), TestDataReader.getTestData(TESTDATA_USER_REGISTRATIONEMAIL), TestDataReader.getTestData(TESTDATA_USER_LASTNAME));
+    public static User InvalidPassword(){
+    return new User(TestDataReader.getTestData(TESTDATA_USER_REGISTRATIONEMAIL), TestDataReader.getTestData(TESTDATA_USER_LASTNAME));
     }
 }
