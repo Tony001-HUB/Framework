7acd9cc2c7570049000b467f22fc87a4548b20c8
Osipenko Anton <hguihggjjh@gmail.com>
2020-12-30 13:37:32 +0300

    Fixed Framework

--- Framework/src/test/java/page/AddToCompareProductsPage.java
+++ Framework/src/test/java/page/AddToCompareProductsPage.java
@@ -8,9 +8,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import service.TestDataReader;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class AddToCompareTwoProductsPage extends AbstractPage {
+public class AddToCompareProductsPage extends AbstractPage {
 
     private static final String OneProductForm_URL = TestDataReader.getTestData("test.data.OneProduct.link");
     private static final String TwoProductForm_URL = TestDataReader.getTestData("test.data.TwoProduct.link");
@@ -25,16 +26,25 @@ public class AddToCompareTwoProductsPage extends AbstractPage {
     @FindBy(xpath = "/html/body/div[3]/header/div[3]/div[4]/a")
     private WebElement openСomparisonPageButton;
 
+    @FindBy(xpath="//*[@id=\"product-comparison\"]/thead/tr/td[1]/a")
+    private WebElement deleteProductButton;
+
+    @FindBy(xpath="/html/body/div[4]/aside[2]/div[2]/footer/button[2]")
+    private WebElement agreeWithDeleteProductButton;
+
+    @FindBy(className = "product-item-photo")
+    private List<WebElement> generalSearchResult;
+
     @FindBy(id = "btn-cookie-allow")
     private WebElement cookieAllowButton;
 
-    public AddToCompareTwoProductsPage()
+    public AddToCompareProductsPage()
     {
         super(DriverSingleton.getDriver());
     }
 
     @Override
-    public AddToCompareTwoProductsPage openPage()
+    public AddToCompareProductsPage openPage()
     {
         driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
         this.driver.get(OneProductForm_URL);
@@ -43,7 +53,7 @@ public class AddToCompareTwoProductsPage extends AbstractPage {
         return this;
     }
 
-    public AddToCompareTwoProductsPage openPageSecondItem()
+    public AddToCompareProductsPage openPageSecondItem()
     {
         driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
         this.driver.get(TwoProductForm_URL);
@@ -51,7 +61,7 @@ public class AddToCompareTwoProductsPage extends AbstractPage {
         return this;
     }
 
-    public AddToCompareTwoProductsPage addingCompareFirstProduct()
+    public AddToCompareProductsPage addingCompareFirstProduct()
     {
         waitForElementToBeClickable(this.driver, By.id("productInfoToCart"));
         addProductButton.click();
@@ -60,7 +70,7 @@ public class AddToCompareTwoProductsPage extends AbstractPage {
         return this;
     }
 
-    public AddToCompareTwoProductsPage addingCompareSecondProduct()
+    public AddToCompareProductsPage addingCompareSecondProduct()
     {
         waitForElementToBeClickable(this.driver, By.id("productInfoToCart"));
         addProductButton.click();
@@ -69,14 +79,29 @@ public class AddToCompareTwoProductsPage extends AbstractPage {
         return this;
     }
 
-    public ProductComparisonPage openProductComparisonPage()
+    public AddToCompareProductsPage openProductComparisonPage()
     {
         openСomparisonPageButton.click();
         logger.error("Transition to the product comparison page");
 
-        return new ProductComparisonPage();
+        return this;
     }
 
+
+    public AddToCompareProductsPage deleteProduct()
+    {
+        deleteProductButton.click();
+        agreeWithDeleteProductButton.click();
+        logger.error("The product was successfully deleted");
+
+        return this;
+    }
+
+    public int countGeneralNumberOfSearchResults()
+    {
+        System.out.println("Number of results:" + generalSearchResult.size());
+        return  generalSearchResult.size();
+    }
 
 
 
--- Framework/src/test/java/page/HomePage.java
+++ Framework/src/test/java/page/HomePage.java
@@ -2,6 +2,7 @@ package page;
 
 import driver.DriverSingleton;
 import model.Product;
+import model.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
@@ -11,12 +12,49 @@ import org.openqa.selenium.support.FindBy;
 import service.TestDataReader;
 
 public class HomePage extends AbstractPage{
+    //*[@id="authorizationPopupContainer"]/[@type="submit"]
+//loginPassword
+//*[@class="header-top-links__link js-authorization-link"]
 
     private static final String HomeForm_URL = TestDataReader.getTestData("test.data.HomeForm.link");
     private final Logger logger = LogManager.getRootLogger();
 
+
+    @FindBy(id = "registerEmail")
+    private WebElement regEmail;
+
+    @FindBy(id = "registerPassword")
+    private WebElement regPassword;
+
+    @FindBy(id = "firstname")
+    private WebElement regFirstname;
+
+    @FindBy(id = "lastname")
+    private WebElement regLastname;
+
+    @FindBy(id = "registerPassword-error")
+    private WebElement regError;
+
+    @FindBy(id = "loginEmail")
+    private WebElement logEmail;
+
+    @FindBy(id = "loginPassword")
+    private WebElement logPassword;
+
     @FindBy(id="_evidon-accept-button")
     private WebElement agreeSiteRulesButton;
+
+    @FindBy(id="nav__search-form-input")
+    private WebElement inputSearch;
+
+    @FindBy(xpath = "//*[@class=\"header-top-links__link js-authorization-link\"]")
+    private WebElement clickLogIn;
+
+    @FindBy(xpath = "//*[@id=\"page-header\"]/div[2]/div/div[3]/a[2]")
+    private WebElement clickRegistration;
+
+    @FindBy(xpath = "//*[@id=\"authorizationPopupContainer\"]/div/div[1]/form/div[4]")
+    private WebElement loginError;
 
     @FindBy(xpath = "//*[@id=\"main-header\"]/nav/div[3]/div/div[2]/button/span[1]/i")
     private WebElement activateButtonSearch;
@@ -24,11 +62,19 @@ public class HomePage extends AbstractPage{
     @FindBy(xpath = "//*[@id=\"search_mini_form\"]/div[2]/button")
     private WebElement clickButtonSearch;
 
-    @FindBy(id="nav__search-form-input")
-    private WebElement inputSearch;
-
     @FindBy(xpath="//*[@id=\"groupMenu\"]/li[1]/a/span/span")
     private WebElement goToCamerasButton;
+
+    @FindBy(xpath="//*[@type=\"submit\"]")
+    private WebElement submitButton;
+
+    @FindBy(xpath="//*[@id=\"authorizationPopupContainer\"]/div/div[1]/div[1]")
+    private WebElement checkLoginLocation;
+
+    @FindBy(xpath="//*[@id=\"authorizationPopupContainer\"]/div/div[2]/div[2]")
+    private WebElement checkRegLocation;
+
+
 
 
     public HomePage()
@@ -62,4 +108,88 @@ public class HomePage extends AbstractPage{
         return new TestingSearchOptionsPage();
     }
 
+    public HomePage openRegistrationForm()
+    {
+        clickRegistration.click();
+
+        return this;
+    }
+
+    public HomePage inputRegFirstname(User user)
+    {
+        regFirstname.sendKeys(user.getUserName());
+
+        return this;
+    }
+
+    public HomePage inputRegLastname(User user)
+    {
+        regLastname.sendKeys(user.getUserLastName());
+
+        return this;
+    }
+
+    public HomePage inputRegLogin(User user)
+    {
+        regEmail.sendKeys(user.getUserEmail());
+
+        return this;
+    }
+
+    public HomePage inputRegPassword(User user)
+    {
+        regPassword.sendKeys(user.getUserPassword());
+
+        return this;
+    }
+
+    public HomePage openLoginForm()
+    {
+        clickLogIn.click();
+
+        return this;
+    }
+
+    public HomePage inputLogin(User user)
+    {
+        logEmail.sendKeys(user.getUserEmail());
+
+        return this;
+    }
+
+
+    public HomePage inputPassword(User user)
+    {
+        logPassword.sendKeys(TestDataReader.getTestData("testdata.user.loginPassword"));
+
+        return this;
+    }
+
+    public HomePage submitButtonClick()
+    {
+        submitButton.click();
+        waitForElementToBeVisibility(this.driver, By.xpath("//*[@id=\"authorizationPopupContainer\"]/div/div[1]/form/div[4]"));
+
+        return this;
+    }
+
+    public String getLoginMessage()
+    {
+        return loginError.getText();
+    }
+
+
+    public String getRegMessage(){ return regError.getText(); }
+
+    public String getLoginLocation()
+    {
+        return checkLoginLocation.getText();
+    }
+
+    public String getRegLocation()
+    {
+        return checkRegLocation.getText();
+    }
+
+    public String getCurrentUrl(){ return HomeForm_URL; }
 }
